b68c744cc2b7501cc69be89610d5650a388f223d
muhiuddinshanto <muhiuddinshanto@gmail.com>
2026-08-10 20:00:55 +0600

    routes updated

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getTasks, createTask, updateTask, deleteTask } from '../controllers/task.controller';
+import { getTasks, getTaskById, createTask, updateTask, deleteTask } from '../controllers/task.controller';
 
 const router = Router();
 
 router.get('/', getTasks);
+router.get('/:id', getTaskById);
 router.post('/', createTask);
 router.patch('/:id', updateTask);
 router.delete('/:id', deleteTask);
